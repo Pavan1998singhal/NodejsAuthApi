8ee545aff6d4110bab73df733abf256bd029e712
Pavan Singhal <Pavan.Singhal@smartcontrols.in>
2020-06-03 15:18:42 +0530

    test get method of /api

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
-console.log('Pavan Singhal');
+const app = express();
+
+app.get('/api', (req,res) => {
+    res.json({
+        message: 'Welcome to the api'
+    });
+})
+
+app.listen(5000, () => console.log('Server has started on port 5000'));
